34276d866a81d88c7e83b8387803e9c736876fb7
wael <rainyxd321@gmail.com>
2026-09-02 20:08:44 +0300

    just registered my first Item which is nitric acid

--- src/main/java/rainy/explosive/MyItems.java
+++ src/main/java/rainy/explosive/MyItems.java
@@ -7,6 +7,9 @@ import net.minecraft.util.Identifier;
 
 public class MyItems {
 
+    public static final Item NITRIC_ACID = registerItem("nitric_acid", new Item(new Item.Settings()));
+
+
     private static Item registerItem(String name, Item item) {
 
         return Registry.register(Registries.ITEM, Identifier.of(Explosive.MOD_ID, name), item);
