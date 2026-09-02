a07531ff6b3898bd49c4948d63f2b6808570e8b7
wael <rainyxd321@gmail.com>
2026-09-02 19:17:14 +0300

    go

---
+++ src/main/java/rainy/explosive/ExplosiveDataGenerator.java
@@ -0,0 +1,11 @@
+package rainy.explosive;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExplosiveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
