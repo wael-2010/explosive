a89526fbeff9062485762d9c6d359fe6f4b845f9
wael <rainyxd321@gmail.com>
2026-09-02 21:35:05 +0300

    i added two more new items: sponge explosive and dirt explosive

--- src/main/java/rainy/explosive/MyItems.java
+++ src/main/java/rainy/explosive/MyItems.java
@@ -11,6 +11,13 @@ public class MyItems {
 
     public static final Item NITRIC_ACID = registerItem("nitric_acid", new Item(new Item.Settings()));
 
+    public static final Item SULFRUIC_ACID = registerItem("sulfuric _acid", new Item(new Item.Settings()));
+
+    public static final Item SPONGE_EXPLOSIVE = registerItem("sponge_explosive", new Item(new Item.Settings()));
+
+    public static final Item DIRT_EXPLOSIVE = registerItem("stone_explosive", new Item(new Item.Settings()));
+
+
 
     private static Item registerItem(String name, Item item) {
 
@@ -19,6 +26,7 @@ public class MyItems {
     public static void registerMyItems(){
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(NITRIC_ACID);
+            entries.add(SULFRUIC_ACID);
         });
     }
 
