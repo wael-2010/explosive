aa20d8be0870c9cd6dcd0178b64ffcbce2c41f96
wael <rainyxd321@gmail.com>
2026-09-11 01:33:14 +0300

    made my first item usable well enough - DIRT ABILITY TO link it with Dirt explosive to blow EVERYTHING but DIRT making

---
+++ src/main/java/rainy/Ability/DirtAbility.java
@@ -0,0 +1,92 @@
+package rainy.Ability;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class DirtAbility extends Item {
+    private static final int CHARGE = 60; // idk i might INCREASE THIS IN FUTURE PLS REMEMBER FUTURE ME
+    private static final int RADIUS =  5;
+
+    public DirtAbility(Settings settings) {
+
+        super(settings);
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 72000; // i didnot really get how this work , BUT IM supposed to make it longer than the charge time
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        player.setCurrentHand(hand);
+
+        return TypedActionResult.consume(player.getStackInHand(hand));
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (world.isClient()) {
+            return;
+        }
+        int POWERINGUP = getMaxUseTime(stack, user) - remainingUseTicks;
+
+        if (POWERINGUP >= CHARGE) {
+            explode((ServerWorld) world, user);
+            user.clearActiveItem();
+
+            if (user instanceof PlayerEntity player) {
+                stack.decrement(1);
+            }
+        }
+
+    }
+
+    private void explode(ServerWorld world, LivingEntity user) {
+        BlockPos center = user.getBlockPos();
+
+        for (int x = -RADIUS; x <= RADIUS; x++) {
+            for (int y = -RADIUS; y <= RADIUS; y++) {
+                for (int z = -RADIUS; z <= RADIUS; z++) {
+                    double square = x * x + y * y + z * z;
+
+                    if (square > RADIUS * RADIUS) {
+                        continue;
+                    }
+
+                    BlockPos pos = center.add(x, y, z);
+
+                    if (world.getBlockState(pos).isAir()) {
+                        continue;
+                    }
+
+                    if (world.getBlockState(pos).getBlock() == Blocks.DIRT) {
+                        continue;
+                    }
+
+                    if (pos.equals(center)) {
+                        continue;
+                    }
+
+                    world.breakBlock(pos, true, user);
+                }
+            }
+        }
+
+        world.createExplosion(null, user.getX(), user.getY(), user.getZ(), 0.0F, World.ExplosionSourceType.NONE);
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+}
--- src/main/java/rainy/explosive/MyItems.java
+++ src/main/java/rainy/explosive/MyItems.java
@@ -6,16 +6,19 @@ import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import rainy.Ability.DirtAbility;
 
 public class MyItems {
 
     public static final Item NITRIC_ACID = registerItem("nitric_acid", new Item(new Item.Settings()));
 
-    public static final Item SULFRUIC_ACID = registerItem("sulfuric _acid", new Item(new Item.Settings()));
+    public static final Item NITRIC_POWDER = registerItem("nitric_powder", new Item(new Item.Settings()));
+
+    public static final Item SULFRUIC_ACID = registerItem("sulfuric_acid", new Item(new Item.Settings()));
 
     public static final Item SPONGE_EXPLOSIVE = registerItem("sponge_explosive", new Item(new Item.Settings()));
 
-    public static final Item DIRT_EXPLOSIVE = registerItem("stone_explosive", new Item(new Item.Settings()));
+    public static final Item DIRT_EXPLOSIVE = registerItem("dirt_explosive", new DirtAbility(new Item.Settings()));
 
 
 
@@ -27,6 +30,9 @@ public class MyItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(NITRIC_ACID);
             entries.add(SULFRUIC_ACID);
+            entries.add(NITRIC_POWDER);
+            entries.add(DIRT_EXPLOSIVE);
+            entries.add(SPONGE_EXPLOSIVE);
         });
     }
 
