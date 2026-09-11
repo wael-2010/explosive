4771cce805cd2b60575d48b4a786f760544db2ef
wael <rainyxd321@gmail.com>
2026-09-11 21:39:34 +0300

    added Recipes for everything and fixed the sulfuric naming as i had it sulfurc in same files

--- src/main/java/rainy/Ability/SpongeAbility.java
+++ src/main/java/rainy/Ability/SpongeAbility.java
@@ -1,78 +1,74 @@
-    package rainy.Ability;
+package rainy.Ability;
 
-    import net.minecraft.block.Blocks;
-    import net.minecraft.entity.LivingEntity;
-    import net.minecraft.entity.player.PlayerEntity;
-    import net.minecraft.item.Item;
-    import net.minecraft.item.ItemStack;
-    import net.minecraft.server.world.ServerWorld;
-    import net.minecraft.util.Hand;
-    import net.minecraft.util.TypedActionResult;
-    import net.minecraft.util.UseAction;
-    import net.minecraft.util.math.BlockPos;
-    import net.minecraft.world.World;
-    import org.apache.logging.log4j.core.jmx.Server;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-    public class SpongeAbility extends Item {
+public class SpongeAbility extends Item {
 
-        private static final int CHARGE = 40;
-        private static final int RADIUS = 5;
-        public SpongeAbility(Settings settings) {
-            super(settings);
+    private final int CHARGE = 40;
+    private static int RADIUS = 10;
+
+    public SpongeAbility(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 9999;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        player.setCurrentHand(hand);
+        return TypedActionResult.consume(player.getStackInHand(hand));
+    }
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (world.isClient()) {
+            return;
         }
+        int LOOOADINGTIMEE = getMaxUseTime(stack, user) - remainingUseTicks;
 
-        @Override
-        public int getMaxUseTime(ItemStack stack, LivingEntity user) {
-            return 9999;
-        }
+        if (LOOOADINGTIMEE >= CHARGE) {
+            drain((ServerWorld) world, user);
 
-        @Override
-        public UseAction getUseAction(ItemStack stack) {
-            return UseAction.BOW;
-        }
-        @Override
-        public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
-            player.setCurrentHand(hand);
-            return TypedActionResult.consume(player.getStackInHand(hand));
-        }
-
-        @Override
-        public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
-            if (world.isClient()) {
-                return;
+            user.clearActiveItem();
+            if (user instanceof PlayerEntity player) {
+                stack.decrement(1);
             }
-            int LOADINGTIMEORSMTH = getMaxUseTime(stack, user) - remainingUseTicks;
-
-            if ( LOADINGTIMEORSMTH >= CHARGE) {
-                drain((ServerWorld) world, user);
-
-                user.clearActiveItem();
-                if(user instanceof PlayerEntity player) {
-                    stack.decrement(1);
-                }
-            }
-        }
-        private void drain(ServerWorld world, LivingEntity user) {
-            BlockPos center = user.getBlockPos();
-                    for (int x = -RADIUS; x <= RADIUS; x++) {
-                        for (int y = -RADIUS; y <= RADIUS; y++) {
-                            for (int z = -RADIUS; z <= RADIUS; z++)  {
-                                double square = x * x + y + y + z * z; // if you think this is useless get a life
-                                 // I added this so the explosion or area of the song is not just a square but close to a sphere or a circle
-                                if (square > RADIUS * RADIUS) {
-                                    continue;
-                                }
-                                BlockPos pos = center.add(x, y , z);
-                                if (world.getBlockState(pos).isAir()) {
-                                    continue;
-                                }
-                                if (!world.getBlockState(pos).isOf(Blocks.WATER)) {
-                                    continue;
-                                }
-                                world.breakBlock(pos, true , user);
-                            }
-                        }
-                    }
-                    world.createExplosion(null, user.getX(), user.getY(), user.getZ(), 0F, World.ExplosionSourceType.NONE);
         }
     }
+    private void drain(ServerWorld world, LivingEntity user) {
+        BlockPos center = user.getBlockPos();
+
+        for (int x = -RADIUS; x <= RADIUS; x++) {
+            for (int y = -RADIUS; y <= RADIUS; y++) {
+                for (int z = -RADIUS; z <= RADIUS; z++) {
+                    double square = x * x + y * y + z * z;
+
+                    if (square > RADIUS * RADIUS) {
+                        continue;
+                    }
+                    BlockPos pos = center.add(x, y , z);
+
+                    if (world.getBlockState(pos).isAir()) {
+                        continue;
+                    }
+                    if (world.getFluidState(pos).isOf(Fluids.WATER)) {
+                        world.setBlockState(pos, Blocks.AIR.getDefaultState());
+                    }
+                }
+
+            }
+        }
+        world.createExplosion(null,user.getX(),user.getY(),user.getZ(),0F,World.ExplosionSourceType.NONE);
+    }}
